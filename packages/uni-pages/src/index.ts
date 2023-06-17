e3f1aadac6d27837f04b9892e72a9a4cf6d1e7f6
Neil Lee <zguolee@foxmail.com>
2023-06-17 22:59:06 +0800

    chore(deps): update

--- packages/uni-pages/src/index.ts
+++ packages/uni-pages/src/index.ts
@@ -1,7 +1,11 @@
 import type { Plugin } from 'vite'
 
 interface UniPagesOptions {
-
+  /**
+   * The default application entry page is the home page
+   * @default 'pages/index' or 'pages/index/index'
+   */
+  homePage?: string
 }
 
 export function VitePluginUniPages(options: UniPagesOptions = {}): Plugin {
